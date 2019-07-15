09cdecdcf9fc3d5bcdd138d649661fb929fcea61
Teja Vemulapati <teja.vemulapati@ericsson.com>
2019-07-15 15:12:18 +0530

    First Git Commit

---
+++ src/main/java/com/qa/utilites/Testutil.java
@@ -0,0 +1,9 @@
+package com.qa.utilites;
+
+public class Testutil {
+	
+	public void testutil() {
+		System.out.println("util method");
+	}
+
+}
---
+++ src/test/java/com/qa/tests/Test1.java
@@ -0,0 +1,8 @@
+package com.qa.tests;
+
+public class Test1 {
+
+	public void tests1() {
+		System.out.println("test1");
+	}
+}
